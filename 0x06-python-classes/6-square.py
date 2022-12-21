c1f7875d1d502dbf6341cc968dff2b9b2516244c
Mulle21 <mulukenab95@gmail.com>
2022-12-20 23:08:37 -0800

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,13 +4,13 @@
 
 class Square:
     """
-    Defining a class square
-    """
+     Defining a class square
+     """
 
-    def __init__(self, size=0, position=(0, 0)):
-        """Initializes the data."""
-        self.size = size
-        self.position = position
+     def __init__(self, size=0, position=(0, 0)):
+         """Initializes the data."""
+         self.size = size
+         self.position = position
 
     @property
     def size(self):
@@ -22,7 +22,7 @@ class Square:
         """Sets the size to a value."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        else value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
@@ -45,6 +45,7 @@ class Square:
     def area(self):
         """Returns the current square area."""
         return self.__size ** 2
+
     def my_print(self):
         """Prints to stdout the square with the character #,
         at the position given by the position attribute.
@@ -54,10 +55,9 @@ class Square:
             return
         for y in range(0, self.__position[1]):
             print()
-        for i in range(0, self.__size):
-            for x in range(0, self.__position[0]):
-                print(" ", end="")
-            for j in range(0, self.__size):
-                print("#", end="")
-            print()
-
+            for i in range(0, self.__size):
+                for x in range(0, self.__position[0]):
+                    print(" ", end="")
+                for j in range(0, self.__size):
+                    print("#", end="")
+                print()
