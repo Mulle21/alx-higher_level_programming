e54277f626939ec63821e105882f89eaa448288a
Mulle21 <mulukenab95@gmail.com>
2023-01-10 05:53:37 -0800

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""Module 9-add_item.
-Adds all arguments to a Python list,
-and then save them to a file.
+"""
+7-add_item module
 """
 
 import sys
@@ -11,17 +10,13 @@ import os.path
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-my_file = 'add_item.json'
+file = "add_item.json"
+json_list = []
 
-my_list = []
+if os.path.exists(file):
+    json_list = load_from_json_file(file)
 
-if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
-    my_list = load_from_json_file(my_file)
+for i in range(1, len(sys.argv)):
+    json_list.append(sys.argv[i])
 
-if len(sys.argv) > 1:
-    for elem in sys.argv[1:]:
-        my_list.append(elem)
-
-save_to_json_file(my_list, my_file)
-
-
+save_to_json_file(json_list, file)
