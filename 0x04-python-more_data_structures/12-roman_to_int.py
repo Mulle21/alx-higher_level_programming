c6046dccd8f869a1820f432925ca857a0b53ad39
Mulle21 <mulukenab95@gmail.com>
2023-01-11 03:11:32 -0800

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
+
+
+
 def roman_to_int(roman_string):
-    """
-    Convert a roman numeral to an integer
-    You can assume the number will be between 1 to 3999.
-    If the roman_string is not a string or None, return 0
-    """
-    if type(roman_string) != str or roman_string is None:
-        return 0
-    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    list = 0
-    for i in range(len(roman_string)):
-        if i > 0 and roman_d[roman_string[i]] > roman_d[roman_string[i - 1]]:
-            list += roman_d[roman_string[i]] - 2 * roman_d[roman_string[i - 1]]
-        else:
-            list += roman_d[roman_string[i]]
-     return list
+    if roman_string and isinstance(roman_string, str):
+        rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        r_s = roman_string
+        numero = 0
+        for l in range(len(r_s)):
+            if l > 0 and (rom[r_s[l]] > rom[r_s[l - 1]]):
+                numero += rom[r_s[l]] - (2 * rom[roman_string[l - 1]])
+            else:
+                numero += rom[r_s[l]]
+        return numero
+    return (0)
