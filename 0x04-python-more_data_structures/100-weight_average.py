da92eab2f1d68cfca5cefd178baa211f0111fdae
Mulle21 <mulukenab95@gmail.com>
2023-01-11 02:50:46 -0800

    task 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -7,4 +7,4 @@ def weight_average(my_list=[]):
     for i in my_list:
         sum_score += i[0] * i[1]
         sum_weight += i[1]
-        return sum_score / sum_weight
+    return sum_score / sum_weight
