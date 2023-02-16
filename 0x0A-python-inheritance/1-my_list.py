730b4a0e918791c3ea845d21bcd0e96801d89f47
Mulle21 <mulukenab95@gmail.com>
2023-02-16 03:07:35 -0800

    task 0-101

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,3 +9,11 @@ class MyList(list):
     A class to customize the list class
     """
 
+    def print_sorted(self):
+        """
+        Prints a list in ascending order
+        Sort a list and then prints on the output
+        """
+
+        if issubclass(MyList, list):
+            print(sorted(self))
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -8,3 +8,9 @@ def is_same_class(obj, a_class):
         obj (any): The object to check.
         a_class (type): The class to match the type of obj to.
     Returns:
+        If obj is exactly an instance of a_class - True.
+        Otherwise - False.
+    """
+    if type(obj) == a_class:
+        return True
+    return False
