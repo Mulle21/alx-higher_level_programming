0858e4b087ce0a144aecf883e1b8fd40d0dc45c1
Mulle21 <mulukenab95@gmail.com>
2023-11-13 23:01:32 -0800

    task 10

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// defines function that converts a number from base 10 to `base`
+exports.converter = function (base) {
+  return function (value) {
+    return value.toString(base);
+  };
