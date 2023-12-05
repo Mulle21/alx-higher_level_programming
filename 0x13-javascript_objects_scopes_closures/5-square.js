2b6f6eb3c8cab8343fa585ccf35732279aef0a8d
Mulle21 <mulukenab95@gmail.com>
2023-12-04 23:54:47 -0800

    task 5

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,28 +1,5 @@
 #!/usr/bin/node
-// create class `Square` which inherits from `Rectangle` but only takes one arg
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-
-  rotate () {
-    [this.width, this.height] = [this.height, this.width];
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
@@ -30,3 +7,4 @@ class Square extends Rectangle {
   }
 }
 
+module.exports = Square;
