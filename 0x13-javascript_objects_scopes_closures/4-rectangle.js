e7e4951cb4aba34c1e6ca892a05c1f0d949c846b
Mulle21 <mulukenab95@gmail.com>
2023-12-04 23:52:58 -0800

    task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-// create class `Rectangle` w/ input filter; print, rotate, and double methods
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
   print () {
@@ -19,8 +15,6 @@ class Rectangle {
   }
 
   double () {
-    this.width *= 2;
-    this.height *= 2;
+    [this.width, this.height] = [this.width * 2, this.height * 2];
   }
-}
-
+};
