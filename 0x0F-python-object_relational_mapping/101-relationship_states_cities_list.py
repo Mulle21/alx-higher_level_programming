2942d03244ac12f0dc355f51b400102926faaea7
Mulle21 <mulukenab95@gmail.com>
2023-12-05 05:50:18 -0800

    task 17

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-"""A script that adds object “Louisiana”
-to the database hbtn_0e_6_us"""
-
-
-from sys import argv
-
+# Lists all States and corresponding Cities in the database hbtn_0e_101_usa.
+# Usage: ./101-relationship_states_cities_list.py <mysql username> /
+#                                                 <mysql password> /
+#                                                 <database name>
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import City
 
 if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    import sys
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(engine)
-    new_st = State(name='Louisiana')
-    session.add(new_st)
-    new_state = session.query(State).filter(State.name == 'Louisiana').first()
-    session.commit()
-    print('{}'.format(new_state.id))
-    session.close()
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("    {}: {}".format(city.id, city.name))
