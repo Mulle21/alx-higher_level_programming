9bf5c4dd995452e99b91ff2c341a8adaf9e282a5
Mulle21 <mulukenab95@gmail.com>
2023-12-05 00:05:16 -0800

    task 8

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
-// defines function that prints total number prior arguments plus current arg
-// example of closure
-exports.logMe = (function (item) {
-  let itemCount = -1;
-  return function (item) {
-    itemCount += 1;
-    console.log(itemCount + ': ' + item);
-  };
+let count = 0;
+exports.logMe = function (item) {
+  console.log(`${count++}: ${item}`);
+};
