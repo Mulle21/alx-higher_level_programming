a49ae407fe01f74d9f6ba9eabf100975c0a3a429
Mulle21 <mulukenab95@gmail.com>
2023-12-04 23:59:17 -0800

    task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
-// defines function that returns the total occurances of an element in a list
 exports.nbOccurences = function (list, searchElement) {
-  let count = 0;
-  for (let i = 0; i < list.length; i++) {
-    if (list[i] === searchElement) {
-      count += 1;
-    }
-  }
-  return count;
+  return list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
+};
