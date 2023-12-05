53d0bf9630f4344788e1bca286a47eb6e94603f2
Mulle21 <mulukenab95@gmail.com>
2023-12-04 23:56:45 -0800

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,8 @@
 #!/usr/bin/node
-// create class `Square` which inherits from `Rectangle`; adds charPrint method
-const SquareBase = require('./5-square');
-
-class Square extends SquareBase {
-  constructor (size) {
-    super(size, size);
-  }
-
-  charPrint (c = 'X') {
-    for (let i = 0; i < this.width; i++) {
-      console.log(c.repeat(this.width));
+module.exports = class Square extends require('./5-square') {
+  charPrint (c) {
+    if (c === undefined) { this.print(); } else {
+      for (let i = 0; i < this.height; i++) { console.log(c.repeat(this.width)); }
     }
   }
-}
-
+};
