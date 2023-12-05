927ba03607d2d451d831303a1c1c561330e5da81
Mulle21 <mulukenab95@gmail.com>
2023-12-04 23:48:02 -0800

    task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,8 @@
-
 #!/usr/bin/node
-// create class `Rectangle` that defines a rectangle, with input filtering
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
-}
-
